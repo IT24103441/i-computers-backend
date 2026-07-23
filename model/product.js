261a958a80d9390ac3d511c5e1a63e3bae3b63ba
IT24103441 <it24103441@my.sliit.lk>
2026-07-23 20:34:42 +0530

    fix: change product name field requirement from true to false

--- model/product.js
+++ model/product.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema(
 
                 name: {
                         type: String,
-                        required: true
+                        required: false
                 },
 
                 altNames: {
